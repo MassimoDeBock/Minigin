8b7fad80e380cfce6af9c11e809a836c18c0955e
Massimo De Bock <massimo.de.bock@student.howest.be>
2022-03-16 17:23:15 +0100

    textComponent might work now... i hope

--- Minigin/TextComponent.h
+++ Minigin/TextComponent.h
@@ -1,6 +1,22 @@
 #pragma once
 #include "Component.h"
-class TextComponent : public Component
-{
-};
 
+namespace dae {
+    class Font;
+    class Texture2D;
+    class TextComponent : public Component
+    {
+    public:
+        TextComponent(const std::string& text, const std::shared_ptr<Font>& font);
+
+        void Update() override;
+        void Render() const override;
+        void SetText(const std::string& text);
+        
+    private:
+        bool m_NeedsUpdate;
+        std::string m_Text;
+        std::shared_ptr<Font> m_Font;
+        std::shared_ptr<Texture2D> m_TextTexture;
+    };
+}
